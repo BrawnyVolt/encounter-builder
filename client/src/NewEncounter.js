e334fed08c288c3705ec305a27eabfd10a864da5
Steven <shyfay0@gmail.com>
2021-12-08 22:35:10 -0800

    changed all values for the MonsterBar to numerals to save a fetch on encounter creation

--- client/src/NewEncounter.js
+++ client/src/NewEncounter.js
@@ -5,15 +5,19 @@ function NewEncounter(){
 
     // new button that adds a new bar(appends to original bar and also adds a delete button that removes just that bar and the button itself)
 
-    function addMonsterToEncounter(e){
-        console.log(e.target.value)
+function getAllValues() {
 
-    }
+    //for each class monster-bar, get value and quantity
+    //first, figure out what a monster's id is
+    //then fetch post to join table, with mon_id and quant
+    document.querySelectorAll(".monster-bar").value
+}
 
 
     return (
         <div>
             <h1> Building new encounters, get yer new encounters here</h1>
+            <button onClick={getAllValues}>Build Encounter!</button>
             <MonsterBar />
         </div>
     )
